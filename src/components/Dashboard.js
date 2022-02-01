34c6cfc09d6f377193f63d2f6c1ba74f5ac1d94f
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-01 16:35:23 -0500

    Initial commit. Add basic dashboard functionality with real data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,108 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import axios from "axios";
+
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors"; 
+
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: getTotalInterviews
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: getLeastPopularTimeSlot
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: getMostPopularDay
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: getInterviewsPerDay
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+  };
 
-    return <main className={dashboardClasses} />;
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel = id => {
+    this.setState(prev => ({
+      focused: prev.focused !== null ? null : id
+    }));
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value(this.state)}
+          onSelect={() => this.selectPanel(panel.id)}
+        />
+      ));
+
+
+    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
+
+    return (
+      <main className={dashboardClasses} >
+        {panels}
+      </main>
+    );
   }
 }
 
